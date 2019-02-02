a5f69e6e6fbc084ba4d339e191d53e6551407005
Kathy <kathrcardoza@gmail.com>
2019-02-02 13:27:17 -0800

    adding results from search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
 
-    $("#add-food").on("click", function(event) {
+    $("#add-food").on("click", function (event) {
         event.preventDefault();
+
+        //this gets the options for different recipes
         var ingredient = $("#search-bar").val().trim();
-        var queryURL = "https://www.food2fork.com/api/search?key=6085193110d842cc5f85203d6d4c5756&q=" + ingredient + "&sort=r";
+        var foods = [];
+        var queryURL = "https://www.food2fork.com/api/search?key=9727f5c2adff51679a6bf7e93ae216f6&q=" + ingredient + "&sort=r";
         console.log(queryURL);
-        
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // console.log(response);
-
-            var result = response.data;
+            // console.log(typeof response);
+            $(".jumbotron").empty();
+            var result = JSON.parse(response);
             console.log(result);
+
+            for (var i = 0; i < result.recipes.length; i++) {
+                var title = result.recipes[i].title;
+                console.log(title);
+                
+                var newDiv = $("<div>");
+
+                newDiv.append("<p class='choices'>" + title + "</p>");
+
+                $(".jumbotron").prepend(newDiv);
+                
+
+            }
 
         });
     });
-
     // $(document).on("click", "#add-food", getIngredient);
 
 })
