c199f92545d117732323283ea30bbb49d0ea7d79
Kathy <kathrcardoza@gmail.com>
2019-02-05 19:47:11 -0800

    added recipeID javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         //this gets the options for different recipes
         var ingredient = $("#search-bar").val().trim();
         var foods = [];
-        var queryURL = "https://www.food2fork.com/api/search?key=9727f5c2adff51679a6bf7e93ae216f6&q=" + ingredient + "&sort=r";
+        var queryURL = "https://www.food2fork.com/api/search?key=9727f5c2adff51679a6bf7e93ae216f6&q=" + ingredient + "&sort=r&page=2";
         console.log(queryURL);
 
         $.ajax({
@@ -18,21 +18,38 @@ $(document).ready(function () {
             var result = JSON.parse(response);
             console.log(result);
 
-            for (var i = 0; i < result.recipes.length; i++) {
+            for (var i = 0; i < 10; i++) {
                 var title = result.recipes[i].title;
                 console.log(title);
-                
+                var recipeID = result.recipes[i].recipe_id;
+                // console.log(recipeID);
                 var newDiv = $("<div>");
 
-                newDiv.append("<p class='choices'>" + title + "</p>");
+                newDiv.append("<div class='choices' data-recipe='" + recipeID + "'>" + title + "</div>");
 
                 $(".jumbotron").prepend(newDiv);
-                
-
             }
 
         });
     });
-    // $(document).on("click", "#add-food", getIngredient);
+    $(document).on("click", ".choices", getRecipes);
 
-})
+    function getRecipes () {
+        var recipe = $(this).attr("data-recipe");
+        console.log(recipe);
+
+        var queryURL = "https://www.food2fork.com/api/get?key=9727f5c2adff51679a6bf7e93ae216f6&rId=" + recipe + "&sort=r";
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response) { 
+            
+        })
+    };
+ 
+    // $(document).on("click", "#add-food", getIngredient);
+    // $("#choices").on("click", function () {
+
+    // })
+});
