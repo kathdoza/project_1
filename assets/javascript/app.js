12230d219be2fd51c275d6f557e035bdc55f7d29
Kathy <kathrcardoza@gmail.com>
2019-02-05 20:16:34 -0800

    added info to get recipes and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,48 +6,53 @@ $(document).ready(function () {
         //this gets the options for different recipes
         var ingredient = $("#search-bar").val().trim();
         var foods = [];
-        var queryURL = "https://www.food2fork.com/api/search?key=9727f5c2adff51679a6bf7e93ae216f6&q=" + ingredient + "&sort=r&page=2";
+        var queryURL = "https://www.food2fork.com/api/search?key=6085193110d842cc5f85203d6d4c5756&q=" + ingredient + "&sort=r&page=2";
         console.log(queryURL);
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // console.log(typeof response);
             $(".jumbotron").empty();
             var result = JSON.parse(response);
             console.log(result);
 
             for (var i = 0; i < 10; i++) {
-                var title = result.recipes[i].title;
-                console.log(title);
+                var buttonTitle = result.recipes[i].title;
+                // console.log(title);
                 var recipeID = result.recipes[i].recipe_id;
                 // console.log(recipeID);
-                var newDiv = $("<div>");
-
-                newDiv.append("<div class='choices' data-recipe='" + recipeID + "'>" + title + "</div>");
-
-                $(".jumbotron").prepend(newDiv);
+                var newButton = $("<button>").attr("class", "choices").text(buttonTitle);
+                newButton.attr("data-recipe", recipeID)
+                $(".jumbotron").prepend(newButton);
             }
 
         });
     });
+    // gets recipe ID to get recipes for user's choice on F2F
     $(document).on("click", ".choices", getRecipes);
 
-    function getRecipes () {
+    function getRecipes() {
         var recipe = $(this).attr("data-recipe");
         console.log(recipe);
 
-        var queryURL = "https://www.food2fork.com/api/get?key=9727f5c2adff51679a6bf7e93ae216f6&rId=" + recipe + "&sort=r";
-
+        var queryURL = "https://www.food2fork.com/api/get?key=6085193110d842cc5f85203d6d4c5756&rId=" + recipe + "&sort=r";
+        console.log(queryURL)
         $.ajax({
-            url:queryURL,
+            url: queryURL,
             method: "GET"
-        }).then(function(response) { 
-            
+        }).then(function (response) {
+            var result = JSON.parse(response);
+            console.log(result);
+
+            var imageURL = result.recipe.image_url;
+            console.log(imageURL);
+
+            var ingredients = result.recipe.ingredients;
+            console.log(ingredients);
         })
     };
- 
+
     // $(document).on("click", "#add-food", getIngredient);
     // $("#choices").on("click", function () {
 
