26d0f753b734a20aff5452a191bf33b664de852a
Kathy <kathrcardoza@gmail.com>
2019-02-07 18:49:28 -0800

    adding some js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
         //this gets the options for different recipes
         var ingredient = $("#search-bar").val().trim();
         var foods = [];
-        var queryURL = "https://www.food2fork.com/api/search?key=6085193110d842cc5f85203d6d4c5756&q=" + ingredient + "&sort=r&page=2";
+        var apiKey = "690178e9a1bd7c5314bc05fe6b77a9dd"
+        var queryURL = "https://www.food2fork.com/api/search?key=" + apiKey + "&q=" + ingredient + "&sort=r&page=2";
+
         console.log(queryURL);
 
         $.ajax({
@@ -17,16 +19,15 @@ $(document).ready(function () {
             var result = JSON.parse(response);
             console.log(result);
 
-            for (var i = 0; i < 10; i++) {
+            for (var i = 0; i < result.recipes.length; i++) {
                 var buttonTitle = result.recipes[i].title;
-                // console.log(title);
+                console.log(buttonTitle);
                 var recipeID = result.recipes[i].recipe_id;
                 // console.log(recipeID);
                 var newButton = $("<button>").attr("class", "choices").text(buttonTitle);
                 newButton.attr("data-recipe", recipeID)
                 $(".jumbotron").prepend(newButton);
             }
-
         });
     });
     // gets recipe ID to get recipes for user's choice on F2F
@@ -41,6 +42,7 @@ $(document).ready(function () {
         $.ajax({
             url: queryURL,
             method: "GET"
+
         }).then(function (response) {
             var result = JSON.parse(response);
             console.log(result);
@@ -52,6 +54,35 @@ $(document).ready(function () {
             console.log(ingredients);
         })
     };
+
+    
+    function ingredientsCall() {
+        $.ajax({
+            url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+            headers: {
+                "x-app-id": "9d90687a",
+                "x-app-key": "ce2d2319cdcd23cd6bf1f7cc07da62b9",
+                "x-remote-user-id": 0
+            },
+            "query": "chicken",
+            method: "POST"
+        }).then(function (response) {
+            ;
+            var ingredientResponse = JSON.parse(response);
+            console.log(ingredientResponse);
+        })
+    }
+
+    $(document).on("click", ".choices", getMenuItem);
+
+    function getMenuItem() {
+        var menuItem = $(this).text();
+        // console.log(menuItem);
+
+
+
+
+    }
 
     // $(document).on("click", "#add-food", getIngredient);
     // $("#choices").on("click", function () {
